2275e8c8f3d3626b05dc3d3946fce7d07fd59f27
IM94 <gladiator.pw@gmail.com>
2023-01-12 23:13:34 +0300

    Added vk_request.py which converts digit id to text id, fixed/improved ussing parameters for im_vkapi.

--- im_vkapi.py
+++ im_vkapi.py
@@ -2,12 +2,16 @@ from settings import VK_TOKEN, USER_ID
 import pprint
 import vk_api
 import argparse
+from vk_request import vk_request
+
 
 # Added for running im_vkapi with parameter to command prompt
-# Example: py .\im_vkapi.py -id 12345678
-parsers = argparse.ArgumentParser(description="Choice the parameters")
-parsers.add_argument('-id', default=USER_ID)
-args = parsers.parse_args()
+# Example: py .\im_vkapi.py -id 12345678 -f online
+def parameters():
+    parsers = argparse.ArgumentParser(description="Choice the parameters")
+    parsers.add_argument('-id', default=USER_ID)
+    parsers.add_argument('-f', default="all")
+    return [parsers.parse_args().id, parsers.parse_args().f]
 
 
 class ImVkApi:
@@ -28,7 +32,7 @@ class ImVkApi:
         return self.session.method("friends.getOnline",
                                    {"user_id": self.user_id})
 
-    def get_friends_list(self) -> list:
+    def get_friends_list_with_fullname(self) -> list:
         """Get a list of all friends from user."""
         list_friends = self.session.method("friends.get", {
             "user_id": self.user_id,
@@ -41,10 +45,35 @@ class ImVkApi:
                                  "id": item["id"]})
         return data_friends
 
+    def get_id_list_friends(self) -> list:
+        """Get a list of all friends from user."""
+        list_friends = self.session.method("friends.get", {
+            "user_id": self.user_id,
+            "fields": "nickname"})
+        items = list_friends["items"]
+        data_friends = []
+        for item in items:
+            data_friends.append(item["id"])
+        return data_friends
+
 
 if __name__ == '__main__':
-    imvkapi = ImVkApi(user_id=args.id)
-    # imvkapi = ImVkApi()
-    # pprint.pprint(get_wall_entries())
+    parameters_id = parameters()[0]
+    parameters_f = parameters()[1]
+
+    imvkapi = ImVkApi(user_id=parameters_id)
+
+    # pprint.pprint(imvkapi.get_wall_entries())
     # pprint.pprint(imvkapi.get_online_friends_list())
-    pprint.pprint(imvkapi.get_friends_list())
+    # pprint.pprint(imvkapi.get_friends_list())
+    got_list = []
+    if parameters_f.lower() == "all":
+        # pprint.pprint(imvkapi.get_id_list_friends())
+        got_list = imvkapi.get_id_list_friends()
+    elif parameters_f.lower() == "online":
+        # pprint.pprint(imvkapi.get_online_friends_list())
+        got_list = imvkapi.get_online_friends_list()
+
+    for _ in got_list:
+        print(vk_request(_))
+
--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ LOGIN = os.environ['LOGIN']
 PASSWORD = os.environ['PASSWORD']
 VK_TOKEN = os.environ['VK_TOKEN']
 USER_ID = os.environ['USER_ID']
-
+USER_ID_TEXT = os.environ['USER_ID_TEXT']
